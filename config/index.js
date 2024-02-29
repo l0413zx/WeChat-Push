5ea0e37a6e7d97f84a013e4d1b60d19e57441ad0
刘志兴 <17778815321@189.cn>
2024-02-29 14:46:30 +0800

    fix: 修改模板

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ const params = {
     appid: 'wx66273cf5f868f0be',
     secret: 'c566d6336f65664281122c577f6901a9',
     touser: 'oF5-k6uKV2i_Ey1dwXXAKmm_Espo',
-    template_id: '-nR2K5-iBecdx64K1v1UqX_lLnc8TFUccIsuT4nSzSc',
+    template_id: 'Bpf1jqp0ojCtqf-Izje7ZruF4-lDbdpdsTtrIqxlWbI',
 
     wea_app_id: '97731283',
     wea_app_secret: 'O4i1PTeW',
