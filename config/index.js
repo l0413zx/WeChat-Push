ded5969af9c818861dba5f329a233bfdcf588664
刘志兴 <17778815321@189.cn>
2024-02-29 15:26:13 +0800

    fix: 修改模板

--- config/index.js
+++ config/index.js
@@ -2,7 +2,7 @@ const params = {
     appid: 'wx66273cf5f868f0be',
     secret: 'c566d6336f65664281122c577f6901a9',
     touser: 'oF5-k6uKV2i_Ey1dwXXAKmm_Espo',
-    template_id: 'nSVWB1QZeXcp1cK-a4qF5pbdF7unjz7thQiuR_9oj6A',
+    template_id: 'xeIBm6115hc-phz0jFULWKGfmZ-zwwYGzmotUE1g_CQ',
 
     wea_app_id: '97731283',
     wea_app_secret: 'O4i1PTeW',
